bd6e718d301ac5364e32d3f26417ee805f4eb436
rizwan <iamrizwan40@gmail.com>
2020-06-18 01:32:05 -0700

    added footer

--- views/js/main.js
+++ views/js/main.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     CKEDITOR.replace('editor');
+    document.querySelector('span.hamburger-icon').addEventListener('click', function(e){
+        e.preventDefault()
+        document.querySelector('ul.mobile-menus').classList.toggle('toggle-ul')
+    })
 })
