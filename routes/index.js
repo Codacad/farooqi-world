c1b314404b4a26f38a1364be9cb25be32aa8b475
rizwan <iamrizwan40@gmail.com>
2020-06-19 08:37:30 -0700

    changed button styles

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,19 @@ const express = require('express');
 const Router = express.Router();
 
 Router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {title:"Farookis World"});
 })
-Router.get('/courses',(req, res) => {
-    res.render('courses');
-})
+
 Router.get('/contact',(req, res) => {
-    res.render('contact');
+    res.render('contact', {title:"Contact"});
 })
 
 Router.get('/about',(req, res) => {
-    res.render('about');
+    res.render('about', {title:"About Us"});
+})
+
+Router.get('/create-new-article', (req, res) => {
+    res.render('create-article', {title:"Create Article"})
 })
 
 module.exports =  Router;
--- views/js/main.js
+++ views/js/main.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    CKEDITOR.replace('editor');
     document.querySelector('span.hamburger-icon').addEventListener('click', function(e){
         e.preventDefault()
         document.querySelector('ul.mobile-menus').classList.toggle('toggle-ul')
