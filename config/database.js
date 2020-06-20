24acad65fc64c371cad671ca4d5cd1bfd43de4ba
rizwan <iamrizwan40@gmail.com>
2020-06-20 06:02:44 -0700

    tried to fixed db connectin error

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/farooqi-world', {
     useNewUrlParser:true,
     useUnifiedTopology:true,
