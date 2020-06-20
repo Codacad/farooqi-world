69528b381b6af6fb0b7a5784a3d747cfb32105bf
rizwan <iamrizwan40@gmail.com>
2020-06-20 05:42:13 -0700

    connected to mongodb to create articles

--- models/article.js
+++ models/article.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
+    author:{type:String},
     title:{type:String, required:true},
-    body:{type:String, required:true},
-    author:{type:String}
+    body:{type:String, required:true}
 }, {timestamps:true})
 
 const Article = mongoose.model('Article', articleSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const Router = express.Router();
+const Articles = require('../models/article');
+const Article = require('../models/article');
 
 Router.get('/', (req, res) => {
-    res.render('index', {title:"Farookis World"});
+    Article.find({}, (err, article) => {
+        if(err){
+            res.status(404).send({msg:"Some Errors Occured"})
+        }
+        res.render('index', {
+            title:"Farooki World",
+            article
+        });
+    })
+    
 })
+
+Router.get('/article/:id', (req, res) => {
+    Article.findById(req.params.id, (err, article) => {
+        if(err){
+            res.status(404).send({msg:"Some Errors Occured"})
+        }
+        res.render('article', {
+            title:"Article",
+            article
+        });
+    })
+})
+
 
 Router.get('/contact',(req, res) => {
     res.render('contact', {title:"Contact"});
@@ -17,8 +41,22 @@ Router.get('/create-new-article', (req, res) => {
     res.render('create-article', {title:"Create Article"})    
 })
 
-Router.get('/article', (req, res) => {
-    res.render('article', {title:"Article"});
+// Post Route for creating new article
+Router.post('/create-new-article', (req, res) => {
+    const newArticle = new Article(req.body);
+    newArticle.save((err, article) => {
+        if(err){
+            res.status(404).send('<h1>Some Errors Occured</h1>')
+        }
+        res.redirect('/')
+    })        
 })
-
+Router.get('/api/article', (req, res) => {
+    Article.find({}, (err, article) => {
+        if(err){
+            res.status(404).send({msg:"Some Errors Occured"})
+        }
+        res.send(article)
+    })
+})
 module.exports =  Router;
