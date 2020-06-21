2eb122916681c81bc907a0f7f6d2f8497c25f1d2
rizwan <iamrizwan40@gmail.com>
2020-06-21 00:12:58 -0700

    readded files to gitignore

--- views/js/ckeditor.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    CKEDITOR.replace('editor');
-})
--- views/js/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('span.hamburger-icon').addEventListener('click', function(e){
-        e.preventDefault()
-        document.querySelector('ul.mobile-menus').classList.toggle('toggle-ul')
-    })
-})
