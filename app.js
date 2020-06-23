c0bde45b3a4258a78f1abca8951d1a1530142b90
rizwan <iamrizwan40@gmail.com>
2020-06-23 05:03:51 -0700

    implemented sessions and session store with connect-mongo

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const session = require('express-session');
+const connectMongo = require('connect-mongo')(session);
 const expressLayout = require('express-ejs-layouts');
 const cors = require('cors');
-const routes = require('./routes/index')
+const routes = require('./routes/index');
 const config = require('dotenv').config()
-
 const app = express()
 app.use(cors());
 app.use(express.json())
@@ -13,8 +14,32 @@ app.set('view engine', "ejs");
 app.use(expressLayout);
 app.use(express.static('views'));
 
-// DB Connection
-require('./config/database')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/farooqi-world', {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+const connection = mongoose.connection;
+connection.on('connected', () => {
+    console.log(`Connection is established at ${connection.host}:${connection.port}`)
+})
+
+const sessionStore = new connectMongo({
+    mongooseConnection:connection,
+    collection:"sessions"
+})
+
+app.use(session({
+    secret:"nayriz",
+    resave:false,
+    saveUninitialized:true,
+    store:sessionStore,
+    cookie:{
+        maxAge: 1000*60*60*24
+    }
+}))
+
+
 app.use('/', routes);
 
 const PORT = process.env.PORT || 5000;
--- config/database.js
+++ config/database.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const session = require('express-session');
+const connectMongo = require('connect-mongo')(session);
+const app = require('express')();
 require('dotenv').config()
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/farooqi-world', {
     useNewUrlParser:true,
@@ -10,4 +13,19 @@ db.on('connected', () => {
     console.log(`Connection is established at ${db.host}:${db.port}`)
 })
 
-module.exports = db;
+const sessionStore = new connectMongo({
+    mongooseConnection:db,
+    collection:"session"
+})
+
+app.use(session({
+    secret:"nayriz",
+    resave:false,
+    saveUninitialized:true,
+    store:sessionStore,
+    cookie:{
+        maxAge: 1000*60*60*24
+    }
+}))
+
+module.exports =  db;
