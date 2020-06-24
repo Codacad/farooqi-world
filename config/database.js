db1255e1eeaeb8994a84b2b71ff95514130c28f1
rizwan <iamrizwan40@gmail.com>
2020-06-24 04:29:28 -0700

    tryig to fix can't find module error

--- config/database.js
+++ config/database.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose');
-const session = require('express-session');
-const connectMongo = require('connect-mongo')(session);
-const app = require('express')();
-require('dotenv').config()
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/farooqi-world', {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-});
+// const mongoose = require('mongoose');
+// const session = require('express-session');
+// const connectMongo = require('connect-mongo')(session);
+// const app = require('express')();
+// require('dotenv').config()
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/farooqi-world', {
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// });
 
-const db = mongoose.connection;
-db.on('connected', () => {
-    console.log(`Connection is established at ${db.host}:${db.port}`)
-})
+// const db = mongoose.connection;
+// db.on('connected', () => {
+//     console.log(`Connection is established at ${db.host}:${db.port}`)
+// })
 
-const sessionStore = new connectMongo({
-    mongooseConnection:db,
-    collection:"session"
-})
+// const sessionStore = new connectMongo({
+//     mongooseConnection:db,
+//     collection:"session"
+// })
 
-app.use(session({
-    secret:"nayriz",
-    resave:false,
-    saveUninitialized:true,
-    store:sessionStore,
-    cookie:{
-        maxAge: 1000*60*60*24
-    }
-}))
+// app.use(session({
+//     secret:"nayriz",
+//     resave:false,
+//     saveUninitialized:true,
+//     store:sessionStore,
+//     cookie:{
+//         maxAge: 1000*60*60*24
+//     }
+// }))
 
-module.exports =  db;
+// module.exports =  db;
 
