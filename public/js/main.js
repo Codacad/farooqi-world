e04dd7e06d01b7a74d067b91cf7907b569b2e483
rizwan <iamrizwan40@gmail.com>
2020-06-27 05:44:21 -0700

    tried to fixed responve overflow

--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
     hamburgerIcon.addEventListener('click', function(e){
         e.preventDefault()
         document.querySelector('nav#sidenav').classList.add('sidenav-toggle');
-        document.querySelector('div.articles').classList.add('overflow-hidden');
+        document.querySelector('body').style.overflow = "hidden";
+        document.querySelector('body').style.transition= "400ms ease-in-out";
         console.log(e.target)
     })
 
     document.querySelector('nav#sidenav a#close').addEventListener('click', (e) => {
         e.preventDefault();
         document.querySelector('nav#sidenav').classList.remove('sidenav-toggle');
-        document.querySelector('div.articles').classList.remove('overflow-hidden');
+        document.querySelector('body').style.overflow = "auto";
+  
     }) 
 })
