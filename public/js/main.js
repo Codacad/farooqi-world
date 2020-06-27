3085b8bac98e39b853c38d8dbbd097e50ea25449
rizwan <iamrizwan40@gmail.com>
2020-06-27 05:00:00 -0700

    added public folder

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerIcon = document.querySelector('span.hamburger-icon');
+    
+    hamburgerIcon.addEventListener('click', function(e){
+        e.preventDefault()
+        document.querySelector('nav#sidenav').classList.add('sidenav-toggle');
+        document.querySelector('div.articles').classList.add('overflow-hidden');
+        console.log(e.target)
+    })
+
+    document.querySelector('nav#sidenav a#close').addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector('nav#sidenav').classList.remove('sidenav-toggle');
+        document.querySelector('div.articles').classList.remove('overflow-hidden');
+    }) 
+})
