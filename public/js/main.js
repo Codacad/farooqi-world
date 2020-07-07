6128597b68d098cc98d4a1d850cfee933a64aca5
rizwan <iamrizwan40@gmail.com>
2020-07-07 07:23:07 -0700

    toggles some classes

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerIcon = document.querySelector('span.hamburger-icon');
+    const hamburgerIcon = document.querySelector('div.nav-left-menus a.hamburger-icon');
     
     hamburgerIcon.addEventListener('click', function(e){
         e.preventDefault()
-        document.querySelector('nav#sidenav').classList.add('sidenav-toggle');
+        document.querySelector('div#sidenav-wrapper').classList.add('sidenav-wrapper-toggle')
+        document.querySelector('nav.sidenav').classList.add('sidenav-toggle');
         document.querySelector('body').classList.add("overflow-hidden");
         document.querySelector('body').style.transition= "400ms ease-in-out";
         console.log(e.target)
     })
-    document.querySelector('nav#sidenav a.close').addEventListener('click', (e) => {
+    
+    document.querySelector('nav.sidenav div.header a.close').addEventListener('click', (e) => {
         e.preventDefault();
-        document.querySelector('nav#sidenav').classList.remove('sidenav-toggle');
+        document.querySelector('div#sidenav-wrapper').classList.remove('sidenav-wrapper-toggle')
+        document.querySelector('nav.sidenav').classList.remove('sidenav-toggle');
         document.querySelector('body').classList.remove("overflow-hidden");
     }) 
 
-    const articleText =  document.querySelectorAll('#article-body-text');
+    const caret = document.querySelector('ul.user-menus li a.caret')
+    let trueOrFalse = false;
+    caret.addEventListener('click', (e) => {        
+        e.preventDefault();       
+        trueOrFalse = !trueOrFalse
+        console.log(trueOrFalse)
+        
+        if(trueOrFalse){
+            document.querySelector('ul.user-menus li ul').classList.add('toggle-user-menus')
+        }else{
+            document.querySelector('ul.user-menus li ul').classList.remove('toggle-user-menus')
+        }
+    })
 
+    window.onclick = (e) => {
+        if(e.target.id == "sidenav-wrapper"){
+            document.querySelector('div#sidenav-wrapper').classList.remove('sidenav-wrapper-toggle')
+            document.querySelector('nav.sidenav').classList.remove('sidenav-toggle');
+            document.querySelector('body').classList.remove("overflow-hidden");
+        }        
+    }
+
+    const articleText =  document.querySelectorAll('#article-body-text');
     articleText.forEach(txt => {       
         if (/^[a-zA-Z]+$/.test(txt.innerHTML)) {                                    
             console.log("English")
